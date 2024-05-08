8cf8019db31035af0aab607b22deae37ac351948
Karmanya20 <99268305+Karmanya20@users.noreply.github.com>
2024-05-08 13:40:15 +0530

    Navbar Created

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return(
+        <div className="Navbar">
+            <div className="logo"><a href=""><h2>Foody's Guide</h2></a></div>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Explore</a>
+            </div>
+        </div>
+    );
+}
